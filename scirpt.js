258cb5e4d2536b1784492b1a43ee97cc5676ed01
wangsh <wugcelama19@gmail.com>
2021-06-13 14:46:24 +0545

    first commit

---
+++ scirpt.js
@@ -0,0 +1,16 @@
+//function create.
+
+function itsme(){
+
+    let myFrindName = ['Hari', 'shita', 'sarita', 'Bijaya', 'Nabin'];
+
+     //using for of loop.
+    for(var  i of  myFrindName){
+
+        console.log(myFrindName);
+    }
+}
+
+
+//function call.
+itsme();
